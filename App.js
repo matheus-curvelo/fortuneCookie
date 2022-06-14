bb5b5999b874c8fa9faa2d38e5ebe7446c4388dc
Matheus Curvelo <matheuscurvelo@gmail.com>
2022-06-14 14:44:59 -0300

    Iniciando aplicação

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { View, Text } from 'react-native';
+import styles from './Style';
+
+function App() {
+  return(
+    <View style={styles.container}>
+
+    </View>
+  )
+}
+
+export default App
