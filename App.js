ac4308efceb203018a4415f416ee82d0713cd7f4
Matheus Curvelo <matheuscurvelo@gmail.com>
2022-06-15 11:56:19 -0300

    Finalizando aplicação

--- App.js
+++ App.js
@@ -1,13 +1,56 @@
-import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import styles from './Style';
 
-function App() {
-  return(
-    <View style={styles.container}>
+class App extends Component {
 
-    </View>
-  )
+  constructor(props){
+    super(props)
+    this.state = {
+      image: require('./src/assets/images/fechado.png'),
+      textoFrase: ''
+
+    }
+
+    this.quebraBiscoito = this.quebraBiscoito.bind(this)
+
+    this.frases = [
+      'Siga os bons e aprenda com eles.', 
+      'O bom-senso vale mais do que muito conhecimento.', 
+      'O riso é a menor distância entre duas pessoas.', 
+      'Deixe de lado as preocupações e seja feliz.',
+      'Realize o óbvio, pense no improvável e conquiste o impossível.',
+      'Acredite em milagres, mas não dependa deles.',
+      'A maior barreira para o sucesso é o medo do fracasso.'
+    ]
+  }
+
+  quebraBiscoito() {
+    let random = Math.floor(Math.random() * this.frases.length )
+
+    this.setState({
+      textoFrase: '"' + this.frases[random] + '"',
+      image: require('./src/assets/images/aberto.png'),
+    })
+  }
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <Image style={styles.image} source={this.state.image} />
+
+        <Text style={styles.text}>
+          {this.state.textoFrase}
+        </Text>
+
+        <TouchableOpacity style={styles.buttonArea} onPress={this.quebraBiscoito}>
+          <Text style={styles.buttonText}>Abrir o biscoito</Text>
+        </TouchableOpacity>
+
+      </View>
+    )
+  }
 }
 
 export default App
--- Style.js
+++ Style.js
@@ -3,9 +3,41 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-    }
-  });
+        flex: 1,
+        paddingTop: 100,
+        alignItems: 'center',
+        backgroundColor: '#eedd99'
+    },
 
-  export default styles;
-  
+    image: {
+        width: 200,
+        height: 200,
+    },
+
+    buttonArea: {
+        borderRadius: 20,
+        backgroundColor: '#ff9c97',
+    },
+
+    buttonText: {
+        color: '#ffffff',
+        fontSize: 24,
+        textTransform: 'uppercase',
+        padding: 20,
+    },
+
+    text: {
+        marginVertical: 24,
+        fontSize: 18,
+        color: '#dd7b22',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        paddingHorizontal: 24,
+        lineHeight: 24,
+        minHeight: 60
+        
+    },
+});
+
+export default styles;
+
